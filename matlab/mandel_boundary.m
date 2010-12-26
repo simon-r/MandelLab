4e3ccf40c49fbeab663c3a8117ce01f3ba23dd96
Simone Riva <simone.rva@gmail.com>
2010-12-26 16:34:19 +0100

    mandel_boundary.m

--- matlab/mandel_boundary.m
+++ matlab/mandel_boundary.m
@@ -1,7 +1,21 @@
-function [ output_args ] = mandel_boundary( input_args )
+function [ r ] = mandel_boundary( c , iter )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
+res = zeros( size(c) ) ;
+
+[row col] = size(c) ;
+
+for i = 1:row
+   r = mandel( c(i,:) , iter ) ;
+   indx = find( r==iter ) ;
+   
+   if size(indx,2) ~= 0
+       j=1 ;
+       r = draw_boundary( r , c , i , indx(j) , iter ) ;
+   end
+   
+end
 
 end
 

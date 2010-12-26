3b9b385c0f92da3a2729d07ffe8d7aa1d250b5b9
Simone Riva <simone.rva@gmail.com>
2010-12-26 12:27:20 +0100

    mendel ok

--- matlab/mandel.m
+++ matlab/mandel.m
@@ -1,7 +1,17 @@
-function [ results ] = mandel( points )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ res ] = mandel( c , iter )
+%mandel: compute the mendelbrot set
+%   c: c matrix of complex coordinates
+%   iter: iteractions
 
+z = zeros( size(c) ) ;
+res = ones( size(c) )*iter ;
+v = ones(size(c)) == 1 ;
+
+for i = 1:iter
+    z(v) = z(v).^2 + c(v) ;
+    v = ( abs( z ) < 1.0 ) ;
+    res(res==iter & not(v) ) = i ;
+end;
 
 end
 

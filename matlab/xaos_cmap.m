836b86a4872ed7770385aedc6ce64fc3bea22f9c
Simone Riva <simone.rva@gmail.com>
2010-12-26 21:44:07 +0100

    xaos_cmap.m 2

--- matlab/xaos_cmap.m
+++ matlab/xaos_cmap.m
@@ -2,13 +2,13 @@ function [ c , delta_r , delta_i ] = xaos_cmap( r_size , i_size , xaos_pos )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-a = xaos_pos(1)-xaos_pos(3) ;
-b = xaos_pos(1)+xaos_pos(3) ;
+a = xaos_pos(1)-xaos_pos(3)/2 ;
+b = xaos_pos(1)+xaos_pos(3)/2 ;
 
 r_range = [a b] ;
 
-a = -xaos_pos(2) - xaos_pos(4) ;
-b = -xaos_pos(2) + xaos_pos(4) ;
+a = -xaos_pos(2) - xaos_pos(4)/2 ;
+b = -xaos_pos(2) + xaos_pos(4)/2 ;
 i_range = [a b] ;
 
 [ c , delta_r , delta_i ] = complexmap( r_size , i_size , r_range , i_range ) ;

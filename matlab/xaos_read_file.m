214265640074c57123848b118fd350ca732fcf0a
Simone Riva <simone.rva@gmail.com>
2010-12-27 10:29:01 +0100

    w read xaos file

--- matlab/xaos_read_file.m
+++ matlab/xaos_read_file.m
@@ -4,6 +4,8 @@ function [ m_data ] = xaos_read_file( file_name )
 
 fid = fopen( file_name );
 
+m_data.error = 1 ;
+
 if fid == -1
     m_data.error = -1 ;
     return ;
@@ -12,17 +14,7 @@ end
 tline = fgetl(fid);
 
 while ischar(tline)
-    [tok mat] = regexp(tline , ...
-        '\(view (-{0,1}\d+\.\d+) (-{0,1}\d+\.\d+) (-{0,1}\d+\.\d+) (-{0,1}\d+\.\d+)\)' ...
-        ,'tokens', 'match' ) ;
-    
-    if  size( tok{:} , 2 ) == 4
-        a = str2num(tok{1}{1}) ;
-        b = str2num(tok{1}{2}) ;
-        c = str2num(tok{1}{3}) ;
-        d = str2num(tok{1}{4}) ;
-        m_data.xaos_pos = [a b c d] ;
-    end
+    m_data = match_view( tline , m_data );
     tline = fgetl(fid);
 end
 
@@ -31,4 +23,28 @@ fclose(fid);
 m_data.error = 1 ;
 
 end
+
+
+
+function [ m_data ] = match_view( tline , m_data )
+
+    reg_num = '(\d*\.?)?\d+([Ee][-+]?\d+)?' ;
+    reg_num = '[-\+]?[\d\w\.-\+]+'
+    
+    regular_expr = sprintf(  '\\(view (%s) (%s) (%s) (%s)\\)' , reg_num , reg_num , reg_num , reg_num ) ;
+    
+    [tok mat] = regexp(tline , regular_expr ,'tokens', 'match' ) ;
+    
+    if  size( tok{:} , 2 ) == 4
+        a = str2num(tok{1}{1}) ;
+        b = str2num(tok{1}{2}) ;
+        c = str2num(tok{1}{3}) ;
+        d = str2num(tok{1}{4}) ;
+        m_data.xaos_pos = [a b c d] ;
+    end
+end
+
+
+
+
 

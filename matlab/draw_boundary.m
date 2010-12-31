6762d639aeeec830f1a82ee11eb66acb0e2b6c5d
Simone Riva <simone.rva@gmail.com>
2010-12-31 18:38:34 +0100

    added draw_boundary

--- matlab/draw_boundary.m
+++ matlab/draw_boundary.m
@@ -4,7 +4,7 @@ function [ r ] = draw_boundary( r , c , i , j , th , iter )
 [ m , v ] = build_bound_matrix( c , i , j ) ;
 
 
-while true  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ATTENZIONE 
+while true  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% ATTENZIONE   
     r = mandel( m , iter , v ) ;
 end ;
 
